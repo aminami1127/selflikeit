b8e706d98ee1c687a89a7debb08912d3d1b13d28
aminami1127 <aminami1127@users.noreply.github.com>
2016-04-02 08:17:25 +0000

    CRUD rest api

--- app.js
+++ app.js
@@ -12,11 +12,10 @@ var app = module.exports = koa();
 app.use(logger());
 
 app.use(route.get('/posts/', posts.all));
-// app.use(route.get('/view/posts/', posts.list));
-// app.use(route.get('/posts/:id', posts.fetch));
-// app.use(route.post('/posts/', posts.add));
-// app.use(route.put('/posts/:id', posts.modify));
-// app.use(route.delete('/posts/:id', posts.remove));
+app.use(route.get('/posts/:id', posts.fetch));
+app.use(route.post('/posts/', posts.add));
+app.use(route.put('/posts/:id', posts.modify));
+app.use(route.delete('/posts/:id', posts.remove));
 // app.use(route.options('/', posts.options));
 // app.use(route.trace('/', posts.trace));
 // app.use(route.head('/', posts.head));
--- controllers/posts.js
+++ controllers/posts.js
@@ -8,12 +8,87 @@ var co = require('co');
 
 var posts = wrap(db.get('posts'));
 
-co(function * () {
+co(function* () {
   var posts = yield posts.find({});
 });
 
-module.exports.all = function * all(next) {
+module.exports.all = function* all(next) {
   if ('GET' != this.method) return yield next;
   this.body = yield posts.find({});
 };
 
+module.exports.fetch = function* fetch(id, next) {
+  if ('GET' != this.method) return yield next;
+  // Quick hack.
+  if(id === ""+parseInt(id, 10)){
+    var post = yield posts.find({}, {
+      'skip': id - 1,
+      'limit': 1
+    });
+    if (post.length === 0) {
+      this.throw(404, 'post with id = ' + id + ' was not found');
+    }
+    this.body = yield post;
+  }
+
+};
+
+module.exports.add = function* add(data, next) {
+  if ('POST' != this.method) return yield next;
+  var post = yield parse.json(this, {
+    limit: '1kb'
+  });
+  var inserted = yield posts.insert(post);
+  if (!inserted) {
+    this.throw(405, "The post couldn't be added.");
+  }
+  this.body = 'Done!';
+};
+
+module.exports.modify = function* modify(id, next) {
+  if ('PUT' != this.method) return yield next;
+
+  var data = yield parse.json(this, {
+    limit: '1kb'
+  });
+
+  var post = yield posts.find({}, {
+    'skip': id - 1,
+    'limit': 1
+  });
+
+  if (post.length === 0) {
+    this.throw(404, 'post with id = ' + id + ' was not found');
+  }
+
+  var updated = posts.update(post[0], {
+    $set: data
+  });
+
+  if (!updated) {
+    this.throw(405, "Unable to update.");
+  } else {
+    this.body = "Done";
+  }
+};
+
+module.exports.remove = function* remove(id, next) {
+  if ('DELETE' != this.method) return yield next;
+
+  var post = yield posts.find({}, {
+    'skip': id - 1,
+    'limit': 1
+  });
+
+  if (post.length === 0) {
+    this.throw(404, 'post with id = ' + id + ' was not found');
+  }
+
+  var removed = posts.remove(post[0]);
+
+  if (!removed) {
+    this.throw(405, "Unable to delete.");
+  } else {
+    this.body = "Done";
+  }
+};
